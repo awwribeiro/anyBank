39575c40bb4feb12684d6427a1127835b6c62899
Ana <awwribeiro@gmail.com>
2025-05-06 14:06:27 -0300

    logica-saldo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from "./componentes/banner/banner.component";
 import { NovaTransacaoComponent } from "./componentes/nova-transacao/nova-transacao.component";
+import { TipoTransacao, Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,32 @@ import { NovaTransacaoComponent } from "./componentes/nova-transacao/nova-transa
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'anybank';
+
+  //inicializar propriedades de componentes que serão alteradas com o tempo.
+  // a lista transacoes inicializa vazia, mas será alterada conforme o uso da aplicação.
+  transacoes = signal<Transacao[]>([]);
+
+  processarTransacao(transcao: Transacao) {
+    this.transacoes.update((listaAtual) => [transcao, ...listaAtual]);
+    //update() permite criar um novo valor a partir do atual
+    //função callback (() => {}) recebe a lista atual, nomeada como parâmetro (listaAtual), e retorna o novo valor da lista.
+    //spread operator (...), insere todos os elementos de listaAtual logo após a nova transação (transacao)
+  }
+
+  saldo = computed(() => {
+    return this.transacoes().reduce((acc, transacaoAtual) => {
+      switch (transacaoAtual.tipo) {
+        case TipoTransacao.DEPOSITO:
+          return acc + transacaoAtual.valor;
+        case TipoTransacao.SAQUE:
+          return acc - transacaoAtual.valor;
+
+          default:
+            throw new Error('Tipo de transação não identificado');
+      }
+    }, 0);
+  });
+  //computed: calcula valores com base em signals e reage automaticamente quando os sinais utilizados mudam.
+  //reduce: reduz um array a um único valor, soma todos os valores das transações
+
 }
