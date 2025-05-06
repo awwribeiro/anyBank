3abcc40b46ff55152f4207e2b41d25526dc70630
Ana <awwribeiro@gmail.com>
2025-05-06 10:01:53 -0300

    componente-novaTransacao

--- src/app/componentes/nova-transacao/nova-transacao.component.ts
+++ src/app/componentes/nova-transacao/nova-transacao.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nova-transacao.component.html',
   styleUrl: './nova-transacao.component.css'
 })
 export class NovaTransacaoComponent {
+  valorTransacao = "";
+  tipoTransacao = ""
 
+  aoSubmeter() {
+      console.log(this.valorTransacao)
+      console.log(this.tipoTransacao)
+      this.tipoTransacao = "";
+      this.valorTransacao = "";
+  }
 }
